48e9fda8bfde3cb2f3401beedd6dcd9d6b6f1ed6
= <60821161+Agua-bit@users.noreply.github.com>
2023-09-26 04:23:30 -0500

    juyyyy

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -31,7 +31,7 @@ public class Animal {
             return "Mi nombre es " + this.nombre + ", tengo una edad de " + this.edad + ", habito en " + this.habitat + " y mi genero es " + this.genero;
         }
         else{
-            return "Mi nombre es " + this.nombre + ", tengo una edad de " + this.edad + ", habito en " + this.habitat + " y mi genero es " + this.genero + ", la zona en la que me ubico es " + this.zona + " en el " + this.zona.getZoo.getNombre();
+            return "Mi nombre es " + this.nombre + ", tengo una edad de " + this.edad + ", habito en " + this.habitat + " y mi genero es " + this.genero + ", la zona en la que me ubico es " + this.zona + " en el " + this.zona.getZoo().getNombre();
         }
     }
 
